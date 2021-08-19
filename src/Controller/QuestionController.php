1e662e2f563ddb42bfd1c3dc456139859f24bcb5
andrii <andrii.sukhoi@nocowanie.pl>
2021-08-19 10:24:11 +0200

    playing with twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,10 +6,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     public function homepage()
     {
@@ -23,5 +24,14 @@ class QuestionController
     public function sampleAnnotationAction(): Response
     {
         return new Response('yo from annotation');
+    }
+    
+    /**
+     * @Route ("/twig")
+     * @return Response
+     */
+    public function sampleTwig(): Response
+    {
+        return $this->render('sample.html.twig');
     }
 }
